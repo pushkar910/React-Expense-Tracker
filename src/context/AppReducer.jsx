8698554fbf67950253defbfc23ef0a8786940e63
Pushkar Kumar <kmrpushkar910@gmail.com>
2022-02-02 17:41:12 +0530

    Added features to Add and delete transactions

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,5 +1,20 @@
+//Reducer is just a way to change your state and return to the application
+
 export default (state, action) => {
   switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id != action.payload
+        ),
+      };
+
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
     default:
       return state;
   }
