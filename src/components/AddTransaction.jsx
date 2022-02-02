d48c6cf18566381b47e5dd0a1a7038c2e227565e
Pushkar Kumar <kmrpushkar910@gmail.com>
2022-02-02 17:55:41 +0530

    Updated AddTransaction component as input of amount was having error

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount, //(we did the plus sign becoz else the amount was getting added as a string, thus wasn't able to get the total)
+    };
+
+    addTransaction(newTransaction);
+  };
 
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
@@ -22,15 +36,14 @@ export const AddTransaction = () => {
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
-        <button
-          className="btn"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        >
-          Add transaction
-        </button>
+        <button className="btn">Add transaction</button>
       </form>
     </div>
   );
